d1489c4e9f0cfc9df2bb8a106621e19c76edaed3
HC <11hce@tec.mx>
2020-07-31 13:18:54 -0500

    checkpoint and update

--- src/app/model-viewer/model-viewer.component.ts
+++ src/app/model-viewer/model-viewer.component.ts
@@ -23,22 +23,16 @@ export class ModelViewerComponent{
   
 
   constructor() {
-    let me = this;
+      var tsc = this;
       this.scene = new THREE.Scene();
       this.camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
-      this.scene.background = new THREE.Color( 0xa0a0a0 );
-      
-      var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
-				mesh.rotation.x = - Math.PI / 2;
-				mesh.receiveShadow = true;
-        
-      this.scene.add( mesh );
-      
+
       var loader = new FBXLoader();
+
       loader.load('../assets/brain.fbx', function ( object ) {
+        tsc.scene.add(object)
         object.position.y = 0;
-        object.position.x =0;  
-        me.scene.add( object);
+        object.position.x = 0;  
 
        } );
       
